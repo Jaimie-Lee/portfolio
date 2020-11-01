92124b2556c6b44ae868362773441cf71f057d10
Jaimie-Lee <moncherjmi@gmail.com>
2020-11-01 01:05:42 -0500

    JS: Navbar

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 'use strict'
 
+// Make navbar tranparent when it is scrolling
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', ( )=>{
+    console.log(window.scrollY);
+    if(window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    }
+    else {
+        navbar.classList.remove('navbar--dark');
+    }
+});
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
